41cd8cea0de3600fb41fc3c61a394ce90cbbb130
Uzoma <uzoma.nwanne@gmail.com>
2025-03-16 23:08:40 +0100

    fixed configs

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+
 const router = express.Router();
 
-router.post("/login",login)
-router.post("/logout",logout)
-router.post("/login",signup)
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -30,6 +30,7 @@ export const useAuthStore = create((set, get) => ({
 
   signup: async (data) => {
     set({ isSigningUp: true });
+    console.log(data)
     try {
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
