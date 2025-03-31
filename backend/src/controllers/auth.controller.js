bab0892cfe683eca3b8020a6779d07735030baa5
Uzoma <uzoma.nwanne@gmail.com>
2025-03-31 20:43:31 +0100

    husky test

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
-  const mailer = '1';
+  const mailer2 = '1';
   try {
     if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
