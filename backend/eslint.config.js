9f12816b6ca06e4defa454243cbf10bde40b230a
Uzoma <uzoma.nwanne@gmail.com>
2025-03-31 16:25:28 +0100

    husky test

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -6,8 +6,7 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
   {
     files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser },
-    rules: {},
+    languageOptions: { globals: globals.node },
   },
   {
     files: ["**/*.{js,mjs,cjs}"],
